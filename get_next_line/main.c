947fb0d32da69fbe83d7018e9c9b115636656a6c
Jorge López Puebla <jlpenero2005@gmail.com>
2024-11-30 16:06:31 +0100

    Update

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -15,9 +15,20 @@
 
 int	main(void)
 {
-	int	fd;
+	int		fd;
+	char	*line;
 
-	fd = open("empty.txt", O_RDONLY);
-	printf("%s\n", get_next_line(fd));
+	fd = open("text.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
 	return (0);
 }
