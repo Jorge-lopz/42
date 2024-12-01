ad426d68454dafb922e495fdc07beaff985a5852
Jorge López Puebla <jlpenero2005@gmail.com>
2024-12-01 12:20:35 +0100

    Reformat First Challenge

--- Advent of Code/Day_01/main.py
+++ Advent of Code/Day_01/main.py
@@ -12,18 +12,36 @@
 
 left_col: [int] = []
 right_col: [int] = []
-result: int = 0
 
-with open('input.txt', 'r') as file:
-    for line in file.read().split('\n'):
-        left_col.append(int(line.split('   ')[0]))
-        right_col.append(int(line.split('   ')[1]))
+# Read the input
+def read_input():
+    with open('input.txt', 'r') as file:
+        for line in file.read().split('\n'):
+            left_col.append(int(line.split('   ')[0]))
+            right_col.append(int(line.split('   ')[1]))
 
-left_col.sort()
-right_col.sort()
+    left_col.sort()
+    right_col.sort()
 
-# Sum the differences between the smallest number on the left and right (removing already paired numbers)
+# FIRST PART: Sum the differences between the smallest number on the left and right (removing already paired numbers)
+read_input()
+difference: int = 0
 while left_col and right_col:
-    result += abs(left_col.pop(0) - right_col.pop(0))
+    difference += abs(left_col.pop(0) - right_col.pop(0))
 
-print(result)
+print("\n\033[37mThe sum of the differences is:\033[0m\033[1m", difference)
+
+# SECOND PART: Calculate the frequency of numbers in the right list manually
+read_input()
+right_freq: dict[int, int] = {}
+for num in right_col:
+    if num in right_freq:
+        right_freq[num] += 1
+    else:
+        right_freq[num] = 1
+
+similarity_score = 0
+for num in left_col:
+    similarity_score += num * right_freq.get(num, 0)
+
+print("\n\033[0m\033[37mThe similarity score is:\033[0m\033[1m", similarity_score)
