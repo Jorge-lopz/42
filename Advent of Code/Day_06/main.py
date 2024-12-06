2fa4dec17929959040f815582d45c381a6449422
Jorge López Puebla <jlpenero2005@gmail.com>
2024-12-07 00:24:00 +0100

    Update

--- Advent of Code/Day_06/main.py
+++ Advent of Code/Day_06/main.py
@@ -1,58 +1,80 @@
-# --------------------------------------------------------------------------- #
-#                                                                             #
-#     main.py                                        ::::      ::::::::       #
-#                                                  ++: :+:   :+:    :+:       #
-#     PROJECT: Advent of Code                    #:+   +:+  +:+               #
-#                                              +#++:++#++: +#+                #
-#                                             +#+     +#+ +#+                 #
-#     AUTHOR: Jorge Lopez Puebla             ##+     #+# #+#    #+#           #
-#     LAST UPDATE: 06/12/2024               ###     ###  ########             #
-#                                                                             #
-# --------------------------------------------------------------------------- #
-
 memory: [[str]] = []
 
 # Read input file
 with open('input.txt', 'r') as file:
     for line in file.readlines():
-        memory.append([char for char in line])
+        memory.append([char for char in line.strip()])
 
-# FIRST PART: Get the number of unique cells the guard will visit
+# Constants
 GUARD_CHARS = ['^', '>', 'v', '<']
+DIRECTIONS = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
 
+# Find guard's starting position and orientation
 def get_guard():
     for row, line in enumerate(memory):
         for col, char in enumerate(line):
             if char in GUARD_CHARS:
-                return [row, col]
+                return [row, col], char
 
-guard: [int, int] = get_guard()
-orientation = memory[guard[0]][guard[1]]
+guard, orientation = get_guard()
 
-while 0 <= guard[0] < len(memory) and 0 <= guard[1] < len(memory[0]):
-    memory[guard[0]][guard[1]] = 'X'
-    match orientation:
-        case '^':
-            if memory[guard[0] - 1][guard[1]] in 'X.':
-                guard[0] -= 1
-            else:
-                orientation = '>'
-        case '>':
-            if memory[guard[0]][guard[1] + 1] in 'X.':
-                guard[1] += 1
-            else:
-                orientation = 'v'
-        case 'v':
-            if memory[guard[0] + 1][guard[1]] in 'X.':
-                guard[0] += 1
-            else:
-                orientation = '<'
-        case '<':
-            if memory[guard[0]][guard[1] - 1] in 'X.':
-                guard[1] -= 1
-            else:
-                orientation = '^'
+# Find all possible obstruction positions
+def find_obstruction_positions(memory, guard):
+    positions = []
+    for row in range(len(memory)):
+        for col in range(len(memory[0])):
+            if memory[row][col] == '.' and [row, col] != guard:
+                positions.append((row, col))
+    return positions
 
-unique_cells = sum(line.count('X') for line in memory)
+# Simulate the guard's movement
+def simulate_guard(memory, guard, orientation):
+    visited = set()
+    guard_pos = guard[:]
+    memory_copy = [row[:] for row in memory]
 
-print("\n\033[0m\033[37mThe number of unique cells the guard will visit is:\033[0m\033[1m", unique_cells)
+    while True:
+        state = (tuple(guard_pos), orientation)
+        if state in visited:
+            return True  # Loop detected
+        visited.add(state)
+
+        # Move guard based on orientation
+        dx, dy = DIRECTIONS[orientation]
+        next_pos = [guard_pos[0] + dx, guard_pos[1] + dy]
+
+        # Check if the next position is valid
+        if (
+                0 <= next_pos[0] < len(memory_copy)
+                and 0 <= next_pos[1] < len(memory_copy[0])
+                and memory_copy[next_pos[0]][next_pos[1]] in 'X.'
+        ):
+            guard_pos = next_pos
+        else:
+            # Change orientation
+            orientation = {'^': '>', '>': 'v', 'v': '<', '<': '^'}[orientation]
+
+        # If guard moves outside bounds, stop simulation
+        if not (0 <= guard_pos[0] < len(memory_copy) and 0 <= guard_pos[1] < len(memory_copy[0])):
+            break
+
+    return False  # No loop detected
+
+# Main logic to find valid obstructions
+def find_valid_obstructions(memory, guard, orientation):
+    valid_positions = []
+    possible_positions = find_obstruction_positions(memory, guard)
+
+    for pos in possible_positions:
+        # Place obstruction
+        memory[pos[0]][pos[1]] = '#'
+        if simulate_guard(memory, guard, orientation):
+            valid_positions.append(pos)
+        # Remove obstruction
+        memory[pos[0]][pos[1]] = '.'
+
+    return valid_positions
+
+# Find and print results
+valid_obstructions = find_valid_obstructions(memory, guard, orientation)
+print("\n\033[0m\033[37mThe number of valid obstruction positions is:\033[0m\033[1m", len(valid_obstructions))
