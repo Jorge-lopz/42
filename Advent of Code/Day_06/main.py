7faf693c4d088e110474020fc4f6306efffd2a41
Jorge López Puebla <jlpenero2005@gmail.com>
2024-12-10 12:08:42 +0100

    Update

--- Advent of Code/Day_06/main.py
+++ Advent of Code/Day_06/main.py
@@ -64,58 +64,58 @@ unique_cells = sum(line.count('X') for line in memory)
 print("\n\033[0m\033[37mThe number of unique cells the guard will visit is:\033[0m\033[1m", unique_cells)
 
 # SECOND PART: 1933 Find the number of possible single obstacle positions that make the guard loop forever
-block_count = 0
-
-guard: [int, int] = get_guard()
-step_limit = 10_000  # Optional safeguard to prevent excessive iterations
-
-for position in [[row, col] for row in range(len(memory)) for col in range(len(memory[0]) - 1)]:
-    if position == guard:  # Cannot block the guard position
-        continue
-    get_map()
-    guard: [int, int] = get_guard()
-    orientation = memory[guard[0]][guard[1]]
-    memory[position[0]][position[1]] = '#'
-    
-    visited_positions = []  # Track visited states as a list to identify cycles
-    steps = 0
-    while 0 <= guard[0] < len(memory) and 0 <= guard[1] < len(memory[0]):
-        state = (tuple(guard), orientation)
-
-        # Check for a repeated state forming a cycle
-        if state in visited_positions:
-            cycle_start = visited_positions.index(state)
-            cycle_length = len(visited_positions) - cycle_start
-            # Validate the cycle by ensuring it repeats consistently
-            if visited_positions[-cycle_length:] == visited_positions[cycle_start:]:
-                block_count += 1
-                break
-
-        visited_positions.append(state)
-        steps += 1
-        if steps > step_limit:
-            break  # Prevent excessive iterations in large grids
-
-        match orientation:
-            case '^':
-                if memory[guard[0] - 1][guard[1]] in 'X.':
-                    guard[0] -= 1
-                else:
-                    orientation = '>'
-            case '>':
-                if memory[guard[0]][guard[1] + 1] in 'X.':
-                    guard[1] += 1
-                else:
-                    orientation = 'v'
-            case 'v':
-                if guard[0] + 1 < len(memory) and memory[guard[0] + 1][guard[1]] in 'X.':
-                    guard[0] += 1
-                else:
-                    orientation = '<'
-            case '<':
-                if memory[guard[0]][guard[1] - 1] in 'X.':
-                    guard[1] -= 1
-                else:
-                    orientation = '^'
-
-print("\n\033[0m\033[37mThe number of obstacle positions to make the guard loop forever is:\033[0m\033[1m", block_count)
+# block_count = 0
+#
+# guard: [int, int] = get_guard()
+# step_limit = 10_000  # Optional safeguard to prevent excessive iterations
+#
+# for position in [[row, col] for row in range(len(memory)) for col in range(len(memory[0]) - 1)]:
+#     if position == guard:  # Cannot block the guard position
+#         continue
+#     get_map()
+#     guard: [int, int] = get_guard()
+#     orientation = memory[guard[0]][guard[1]]
+#     memory[position[0]][position[1]] = '#'
+#
+#     visited_positions = []  # Track visited states as a list to identify cycles
+#     steps = 0
+#     while 0 <= guard[0] < len(memory) and 0 <= guard[1] < len(memory[0]):
+#         state = (tuple(guard), orientation)
+#
+#         # Check for a repeated state forming a cycle
+#         if state in visited_positions:
+#             cycle_start = visited_positions.index(state)
+#             cycle_length = len(visited_positions) - cycle_start
+#             # Validate the cycle by ensuring it repeats consistently
+#             if visited_positions[-cycle_length:] == visited_positions[cycle_start:]:
+#                 block_count += 1
+#                 break
+#
+#         visited_positions.append(state)
+#         steps += 1
+#         if steps > step_limit:
+#             break  # Prevent excessive iterations in large grids
+#
+#         match orientation:
+#             case '^':
+#                 if memory[guard[0] - 1][guard[1]] in 'X.':
+#                     guard[0] -= 1
+#                 else:
+#                     orientation = '>'
+#             case '>':
+#                 if memory[guard[0]][guard[1] + 1] in 'X.':
+#                     guard[1] += 1
+#                 else:
+#                     orientation = 'v'
+#             case 'v':
+#                 if guard[0] + 1 < len(memory) and memory[guard[0] + 1][guard[1]] in 'X.':
+#                     guard[0] += 1
+#                 else:
+#                     orientation = '<'
+#             case '<':
+#                 if memory[guard[0]][guard[1] - 1] in 'X.':
+#                     guard[1] -= 1
+#                 else:
+#                     orientation = '^'
+#
+# print("\n\033[0m\033[37mThe number of obstacle positions to make the guard loop forever is:\033[0m\033[1m", block_count)
